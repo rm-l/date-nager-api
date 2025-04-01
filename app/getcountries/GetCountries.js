4b8e3116fef9e598cd4be1ee33afad08d3a36e8e
pie <rm8l@outlook.com>
2025-04-01 17:58:53 -0300

    fix just change main page name

--- app/getcountries/GetCountries.js
+++ app/getcountries/GetCountries.js
@@ -36,7 +36,7 @@ export default function AvalibleCountries() {
   return (
     <div className="flex flex-col min-w-full min-h-screen overflow-auto">
       <h1 className="flex font-bold justify-center w-screen min-h-20 p-5 text-5xl bg-slate-200 border-gray-700 border-2 rounded-sm shadow-md">
-        Available Countries
+        Countries
       </h1>
       <ul className="grid grid-cols-2 min-w-80 min-h-80 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 p-2 justify-evenly">
         {data.map((item) => (
